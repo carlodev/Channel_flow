7d6eb6b2a5a94a746fe7cdb6ce716e9655571e85
carlodev <carlobrunellibs@gmail.com>
2022-04-13 15:49:31 +0100

    Channel stabilization
    fixed issue
    with degree = 1 doesn't work periodic conditions
    degree = 2

--- Channel_2d_3d.jl
+++ Channel_2d_3d.jl
@@ -12,18 +12,18 @@ For D=3, 3d case, wrong velocity results - instability?
 """
 
 # Settings
-periodic = false # If set to false, will put a uniform velocity u_in at the inlet
+periodic = true # If set to false, will put a uniform velocity u_in at the inlet
 u_in = 1.0
-ν = 0.0005 # Kinematic vicosity
-D=2; #add const, dimensions number 2 or 3
+ν = 0.0001472 # Kinematic vicosity
+D=3; #add const, dimensions number 2 or 3
 N=32; #add const, cells per dimensions
 order = 1
 u_0 = u_in
 
 include("Channel_Mesh.jl")
 
-model=mesh_channel(;D=D, N=N, printmodel=false, periodic)
-body_force = periodic ? 0.033 : 0.0
+model=mesh_channel(;D=D, N=N, printmodel=true, periodic)
+body_force = periodic ? 0.00337204 : 0.0
 
 @static if D==2
     top = "tag_5"
@@ -35,8 +35,8 @@ body_force = periodic ? 0.033 : 0.0
     inlet_top = "tag_1"
     inlet_bottom = "tag_3"
     u_diri_tags=[top, bottom]
-    u_walls=VectorValue(0,0)
-    u_in_v = VectorValue(u_in,0)
+    u_walls=VectorValue(0.0,0.0)
+    u_in_v = VectorValue(u_in,0.0)
     u_diri_values = [u_walls, u_walls]
     p_diri_tags=String[]
     p_diri_values=Float64[]
@@ -47,24 +47,24 @@ body_force = periodic ? 0.033 : 0.0
         append!(p_diri_values, [0,0,0])
     end
     
-    body_force = periodic ? 0.033 : 0.0
-    hf(x)=VectorValue(body_force, 0)
+    
+    hf(x)=VectorValue(body_force, 0.0)
     
 elseif D==3
-    top = ["tag_23", "tag_9", "tag_11"] # face right left
+    top = ["tag_23", "tag_09", "tag_11"] # face right left
     bottom = ["tag_24", "tag_10", "tag_12"]
 
     inlet = "tag_05"
-    inlet_corners = ["tag_1", "tag_3", "tag_5", "tag_7"] #topleft bottomleft topright bottomright
+    inlet_corners = ["tag_01", "tag_03", "tag_05", "tag_07"] #topleft bottomleft topright bottomright
     inlet_sides = ["tag_13", "tag_15", "tag_17", "tag_19"] #left right top bottom
 
     outlet = "tag_26"
-    outlet_corners = ["tag_2", "tag_4", "tag_6", "tag_8"] #topleft bottomleft topright bottomright
+    outlet_corners = ["tag_02", "tag_04", "tag_06", "tag_08"] #topleft bottomleft topright bottomright
     outlet_sides = ["tag_14", "tag_16", "tag_18", "tag_20"]
 
     sides = ["tag_21", "tag_22"] # left right
 
-    u_diri_tags=[top, bottom]
+    u_diri_tags=append!(top, bottom)
     u_walls=VectorValue(0, 0, 0)
     u_in_v = VectorValue(u_in,0, 0)
 
@@ -82,18 +82,20 @@ elseif D==3
         append!(p_diri_values, [0,0,0])
     end
 
-    u_diri_tags=["tag_24", "tag_23"]
-    u_walls=VectorValue(0,0,0)
+
     hf(x)=VectorValue(body_force, 0, 0)
 end
 
 reffeᵤ = ReferenceFE(lagrangian,VectorValue{D,Float64},order)
 V = TestFESpace(model,reffeᵤ,conformity=:H1,dirichlet_tags=u_diri_tags)
+reffeₚ = ReferenceFE(lagrangian,Float64, order)
 
-reffeₚ = ReferenceFE(lagrangian,Float64,order)
 if periodic
     Q = TestFESpace(model, reffeₚ, conformity=:H1, constraint=:zeromean)
+
 else
+    
+    reffeₚ = ReferenceFE(lagrangian,Float64,order) 
     Q = TestFESpace(model, reffeₚ, conformity=:H1, dirichlet_tags=p_diri_tags)
 end
 
@@ -107,7 +109,7 @@ end
 Y = MultiFieldFESpace([V, Q])
 X = MultiFieldFESpace([U, P])
 
-degree = order
+degree = 2 
 Ω = Triangulation(model)
 dΩ = Measure(Ω,degree)
 
@@ -134,14 +136,16 @@ end
 
 τb(u,h) = (u⋅u)*τ(u,h)
 
-res((u,p),(v,q)) = ∫(
+var_equations((u,p),(v,q)) = ∫(
     ν*∇(v)⊙∇(u) # Viscous term
     + v⊙Rm(u,p) # Other momentum terms
-    + q*Rc(u) # Continuity
-    + (τ∘(u,h)*(u⋅∇(v) + ∇(q)))⊙Rm(u,p) # First term: SUPG, second term: PSPG
+    + q*Rc(u) )dΩ
+    # Continuity
+stab_equations((u,p),(v,q)) = ∫((τ∘(u,h)*(u⋅∇(v) + ∇(q)))⊙Rm(u,p) # First term: SUPG, second term: PSPG
     + τb∘(u,h)*(∇⋅v)⊙Rc(u) # Bulk viscosity. Try commenting out both stabilization terms to see what happens in periodic and non-periodic cases
 )dΩ
 
+res((u,p),(v,q)) = var_equations((u,p),(v,q)) + stab_equations((u,p),(v,q))
 op = FEOperator(res,X,Y)
 nls = NLSolver(show_trace=true, method=:newton, linesearch=MoreThuente(), iterations=30)
 solver = FESolver(nls)
@@ -161,10 +165,10 @@ writevtk(Ω,"results-channel-d$D",cellfields=["uh"=>uh,"ph"=>ph, "ω"=>ω])
 
 using Plots
 
-nn = 1000
+nn = 100
 ev_nodes = LinRange(-0.999, 0.999, nn)
 y =1
-vt(i) = VectorValue(y,i)
+vt(i) = VectorValue(y,i,0)
 ve(v) = v[1] 
 
 vm = evaluate(uh, vt.(ev_nodes))
---
+++ Postprocess_graph.jl
@@ -0,0 +1,5 @@
+using Plots
+
+function pp_graph(Re_tau::Integer, nu= 0.0001472::Float64)
+    
+end
